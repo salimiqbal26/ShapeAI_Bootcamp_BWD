fd2294c25bb45f115eed2c3716bfe447545a011b
salimiqbal26 <85459485+salimiqbal26@users.noreply.github.com>
2021-06-07 06:59:31 +0500

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Project from ShapeAI</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <h1>Javascript and React.js BootCamp</h1>
+      <p>
+        This is an amazing bootcamp taken by shaurya sinha . we cover everything
+        from scratch including javascript, React.js and HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
